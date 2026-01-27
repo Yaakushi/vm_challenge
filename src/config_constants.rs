087db19d65505a0e9beb4fda5c527eb138263690
Khloe Camargo <khloe@nic.br>
2026-01-27 09:57:04 -0300

    Slight adjustment to constants.

--- src/config_constants.rs
+++ src/config_constants.rs
@@ -1,3 +1,5 @@
 pub const NUM_OF_REGISTERS: usize = 8;
-pub const MEMORY_SIZE: usize = 1 << 15;
 pub const START_OF_REGISTER_INDEX: u16 = 1 << 15;
+
+// Our memory is addressable from 0..START_OF_REGISTER_INDEX.
+pub const MEMORY_SIZE: usize = START_OF_REGISTER_INDEX as usize;
